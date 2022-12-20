6757c02f3a041984401ff589eb8b5ec8c09f65ed
Mubashira Rahman <72703179+MubashiraRahman@users.noreply.github.com>
2022-12-21 00:22:54 +0600

    Add files via upload

---
+++ cond.php
@@ -0,0 +1,11 @@
+<?php
+print("PAYMET POLICY: 
+In accordance with and subject to the provisions of NYSERDA’s Prompt Payment Policy Statement, attached hereto as Exhibit D, NYSERDA shall pay to the Contractor within the prescribed time after receipt of such invoice for final payment, the total amount payable pursuant to Section 4.01 hereof, less all progress payments/milestone payments previously made to the Contractor with respect thereto and subject to the maximum commitment set forth in Section 4.06 hereof.
+In an effort to remove the race- and gender-neutral barrier of the length of time for subcontractor payments on Airport Board procurements, the Airport Board has an Expedited Payment Policy for eligible Contractors that may elect to voluntarily participate in.
+We will invoice you the SaaS Fees and fees for other professional services in the Investment Summary per our Invoicing and Payment Policy, subject to Section E(2).");
+?>
+
+<body background="https://images8.alphacoders.com/568/568490.jpg">
+<center>
+<a href="contact.php"> Agree </a>
+</center>
