212540f99459b8f93ae24c596cb4fda29e6596f8
Mubashira Rahman <72703179+MubashiraRahman@users.noreply.github.com>
2022-12-21 00:19:20 +0600

    Update run.php

--- run.php
+++ run.php
@@ -1,7 +1,14 @@
 <?php
 
-print("Booking complete");
+print("CAR BOOKING POLICY: 
+Under these Conditions and the subsidiary rules of these Conditions prescribed pursuant to Article 40 (hereinafter referred to collectively as  the Company shall rent a motor vehicle (hereinafter referred to the Renter, and the Renter shall rent such Rental Vehicle from the Company upon understanding and agreeing to the Terms and Conditions, etc.
+ If the Renter designates a driver who is not the Renter pursuant to Article 8.3, the Renter shall make known the provisions in the Terms and Conditions, etc. which relate to such driver and cause such driver to comply with such provisions. Particulars not provided in the Terms and Conditions, etc. shall be construed in accordance with the subsidiary rules provided in laws, regulations and general customs.
+ The Company may enter into any special contract, provided that it is not contrary to the purport of the Terms and Conditions, etc., the laws, the administrative notices and general customs. In the event special contracts are entered into, such special contracts shall prevail over the Terms and Conditions, etc..
+ Provided that the Renter agrees to the Terms and Conditions and the price list, etc. as set forth separately, the Renter can, upon renting the Rental Vehicle, by the method provided separately, make reservations by specifying in advance the class of the Rental Vehicle, the commencement date and time of rental, the renting place, the period of rental, the returning place, the driver's name(s), use or non-use of a child seat or other accessories, and other rental conditions (hereinafter referred to as ). With regard to microbuses, the Renter shall make reservations by additionally specifying the driving area or the destination(s), as well as the number of passengers and the purpose of rental, as part of the Renting Conditions.");
 
 ?>
 
-<a href="pay.php"> Proceed to Pay </a>
+<body background="https://images8.alphacoders.com/568/568490.jpg">
+<center>
+<a href="pay.php"> Agree </a>
+</center>
